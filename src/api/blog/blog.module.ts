4d07fb9382464f37699230f1cd4ef68b13e62868
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2025-05-25 14:47:36 +0700

    chore: update folder name

--- src/api/blog/blog.module.ts
+++ src/api/blog/blog.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { BlogService } from './blog.service'
 import { BlogController } from './blog.controller'
 import { MongooseModule } from '@nestjs/mongoose'
-import { Blog, BlogSchema } from './schema/blog.schema'
+import { Blog, BlogSchema } from './schemas/blog.schema'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }])],
