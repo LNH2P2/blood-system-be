8d104971250fc4ba20cfb53bee1ece29ad9b93d4
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-07-07 16:18:17 +0700

    feat: remove unused _id property from AbstractSchema

--- src/database/schemas/abstract.schema.ts
+++ src/database/schemas/abstract.schema.ts
@@ -1,14 +1,8 @@
 import { Prop, Schema } from '@nestjs/mongoose'
-import mongoose, { Document } from 'mongoose'
+import { Document } from 'mongoose'
 
 @Schema()
 export abstract class AbstractSchema extends Document {
-  @Prop({
-    type: String,
-    default: () => new mongoose.Types.ObjectId().toString()
-  })
-  declare _id: string
-
   @Prop()
   createdAt: Date
 
