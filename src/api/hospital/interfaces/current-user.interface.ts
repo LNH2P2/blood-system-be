c1113f840f9d14cfae48e3f081c969f37716f523
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-07-07 19:23:51 +0700

    feat: remove CurrentUser interface to simplify user management

--- src/api/hospital/interfaces/current-user.interface.ts
+++
@@ -1,9 +0,0 @@
-import { UserRole } from '../../users/user-type/enum/user.enum'
-
-export interface CurrentUser {
-  _id: string
-  email: string
-  role: UserRole
-  username?: string
-  fullName?: string
-}
