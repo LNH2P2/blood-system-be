d5a26477bd9ef84ffffc74a4acc377b4d6ab022f
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-07-07 17:39:16 +0700

    feat: remove HospitalStaff schema from HospitalModule to streamline imports and exports

--- src/api/hospital/hospital.module.ts
+++ src/api/hospital/hospital.module.ts
@@ -3,23 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { HospitalService } from './hospital.service'
 import { HospitalController } from './hospital.controller'
 import { Hospital, HospitalSchema } from './schemas/hospital.schema'
-import { HospitalStaff, HospitalStaffSchema } from './schemas/hospital-staff.schema'
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: Hospital.name, schema: HospitalSchema },
-      { name: HospitalStaff.name, schema: HospitalStaffSchema }
-    ])
-  ],
+  imports: [MongooseModule.forFeature([{ name: Hospital.name, schema: HospitalSchema }])],
   controllers: [HospitalController],
   providers: [HospitalService],
-  exports: [
-    HospitalService,
-    MongooseModule.forFeature([
-      { name: Hospital.name, schema: HospitalSchema },
-      { name: HospitalStaff.name, schema: HospitalStaffSchema }
-    ])
-  ]
+  exports: [HospitalService, MongooseModule.forFeature([{ name: Hospital.name, schema: HospitalSchema }])]
 })
 export class HospitalModule {}
