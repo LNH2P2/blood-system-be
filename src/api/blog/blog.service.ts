3e3ba8111ca84ae39669e58bf180071f2dcb48ed
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2025-07-11 21:11:43 +0700

    feat: 🎸 also fetch metrics group by blog status

--- src/api/blog/blog.service.ts
+++ src/api/blog/blog.service.ts
@@ -6,6 +6,7 @@ import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { ListBlogReqDto } from './dto/list-blog.req.dto'
 import { PaginationUtil } from '@utils/pagination.util'
+import { BlogStatus } from './blog.constants'
 
 @Injectable()
 export class BlogService {
@@ -15,14 +16,46 @@ export class BlogService {
     return this.blogModel.create(createBlogDto)
   }
 
-  findAll(listBlogReqDto: ListBlogReqDto) {
-    return PaginationUtil.paginate({
+  async findAll(listBlogReqDto: ListBlogReqDto) {
+    // Get paginated results
+    const paginatedResult = await PaginationUtil.paginate({
       model: this.blogModel,
       pageOptions: listBlogReqDto,
-      searchFields: [],
+      searchFields: ['title', 'summary', 'content'],
       sortField: 'createdAt',
       filter: {}
     })
+
+    // Get status counts using aggregation
+    const statusCounts = await this.blogModel.aggregate([
+      {
+        $group: {
+          _id: '$status',
+          count: { $sum: 1 }
+        }
+      }
+    ])
+
+    // Initialize counts for all statuses
+    const statusCountsMap = {
+      [BlogStatus.DRAFT]: 0,
+      [BlogStatus.PUBLISHED]: 0,
+      [BlogStatus.ARCHIVED]: 0,
+      [BlogStatus.PRIVATE]: 0
+    }
+
+    // Map aggregation results to the initialized object
+    statusCounts.forEach((item) => {
+      if (item._id && item._id in statusCountsMap) {
+        statusCountsMap[item._id] = item.count
+      }
+    })
+
+    // Return both paginated data and status counts
+    return {
+      ...paginatedResult,
+      statusCounts: statusCountsMap
+    }
   }
 
   findOne(id: string) {
